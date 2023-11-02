85be19175d0e802546c810d6180478d880005d1f
kaphaein <cheriffadaly@github.com>
2023-11-02 11:27:53 -0700

    “import”

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,12 +4,14 @@ if __name__ == "__main__":
     import sys
     x = sys.argv
     count = len(x) - 1
-    a = int(x[1])
-    b = int(x[3])
 
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    a = int(x[1])
+    b = int(x[3])
+
     if x[2] == "+":
         print("{} + {} = {}".format(a, b, a + b))
     elif x[2] == "-":
