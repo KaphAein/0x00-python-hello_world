ecf2fc7905418be433bda101da0e39f5f118d48a
kaphaein <cheriffadaly@github.com>
2023-11-02 10:37:50 -0700

    “import”

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    av = sys.argv
-    count = len(av) - 1
-    result = 0
-    if count == 0:
-        print("0")
-    elif count == 1:
-        print("{}".format(av[1]))
-    else:
-        for i in range(count):
-            result += int(av[i + 1])
-    print("{}".format(result))
+    import variable_load_5
+    print("{}".format(variable_load_5.a))
