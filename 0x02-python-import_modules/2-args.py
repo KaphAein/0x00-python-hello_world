336b1709d3884230d3f7dcb824792d9983c38275
kaphaein <cheriffadaly@github.com>
2023-11-02 09:38:21 -0700

    “import”

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
         print("{} arguments.".format(len(argv) - 1))
     elif len(argv) > 1:
         print("{} arguments:".format(len(argv) - 1))
-        for i < len(argv) - 1:
+        for i in range(len(argv - 1)):
             print("{}: {}".format(i + 1, argv[i + 1])
