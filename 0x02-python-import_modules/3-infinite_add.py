07ef340c9b12b78609bc4ee626f0af86a35c0e15
kaphaein <cheriffadaly@github.com>
2023-11-02 10:00:03 -0700

    “import”

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    av = sys.argv
+    count = len(av) - 1
+    result = 0
+    if count == 0:
+        print("0")
+    elif count == 1:
+        print("{:d}".format(av[1]))
+    else:
+        for(i in range(count)):
+            result += int(av[i + 1])
+    print("{:d}".format(result))
