0ba0c503f0e19ca010df8a823e2aaa765c32c7ba
kaphaein <cheriffadaly@github.com>
2023-11-02 12:09:42 -0700

    “import”

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,6 +6,6 @@ def magic_calculation(a, b, c):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
+        return (c)
     else:
-        return(sub(a, b))
+        return (sub(a, b))
