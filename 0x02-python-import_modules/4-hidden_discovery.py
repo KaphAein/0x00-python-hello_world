017f4a27f81cb6aa124d718348bd0c329b94a43d
kaphaein <cheriffadaly@github.com>
2023-11-02 10:26:38 -0700

    “import”

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    av = sys.argv
-    count = len(av) - 1
-    result = 0
-    if count == 0:
-        print("0")
-    elif count == 1:
-        print("{}".format(av[1]))
-    else:
-        for i in range(count):
-            result += int(av[i + 1])
-    print("{}".format(result))
+import hidden_4
+for name in dir(hidden_4):
+    if not name.startswith('__'):
+        print(name)
