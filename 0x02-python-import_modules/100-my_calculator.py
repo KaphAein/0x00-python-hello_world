574301a48ae0a41f67a9bd3ab246e0f96355e61d
kaphaein <cheriffadaly@github.com>
2023-11-02 11:06:38 -0700

    “import”

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     if a[2] == "+":
         print("{} + {} = {}".format(a[1], a[3], (int(a[1]) + int(a[3]))))
     elif a[2] == "-":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a[1], a[3], (int(a[1]) / int(a[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
