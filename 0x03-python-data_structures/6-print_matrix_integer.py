4eaf34381a81efa5ba438c98450d43646a811d7a
kaphaein <cheriffadaly@github.com>
2023-11-04 10:32:34 -0700

    “lists”

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(matrix):
-        for j in range(matrix[i]):
-            print("{}".format(matrix[i][j], end=" "))
+def print_matrix_integer(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j], end=" "))
         print()
