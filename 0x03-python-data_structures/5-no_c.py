319f190d7d29bf2ccf9bdde5a3828caf1183e5fb
kaphaein <cheriffadaly@github.com>
2023-11-04 07:35:28 -0700

    “lists”

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for letter in my_string:
-    my_string.remove('c')
-    my_string.remove('C')
+        if my_string[letter] = 'c' or my_string[letter] = 'C':
+            my_string.remove('c')
+            my_string.remove('C')
     return my_string
