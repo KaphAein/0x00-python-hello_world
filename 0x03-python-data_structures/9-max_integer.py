7f867ee1f82aaec1f44ff9810aaf677f3260c1ea
kaphaein <cheriffadaly@github.com>
2023-11-04 12:15:26 -0700

    “lists”

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
     maxi = my_list[0]
     for i in range(1, len(my_list)):
-        if my_list[i] > maxi
+        if my_list[i] > maxi:
             maxi = my_list[i]
         else:
             continue
