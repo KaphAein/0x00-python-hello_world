156adca6aeefe6d5dd369fd1f3c628f3e8e25b39
kaphaein <cheriffadaly@github.com>
2023-11-04 11:54:59 -0700

    “lists”

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = (len(sentence), sentence[0])
-    return tup
+    if sentence:
+        tup = (len(sentence), sentence[0])
+        return tup
