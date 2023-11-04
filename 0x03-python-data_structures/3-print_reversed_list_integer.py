be54f491bf87377e5a901f8921b6dbbe04e1b758
kaphaein <cheriffadaly@github.com>
2023-11-04 07:05:36 -0700

    “lists”

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     count = len(my_list) - 1
     for i in range(count + 1):
-        if i < count - 1:
+        if i < count - i:
             temp = my_list[i]
             my_list[i] = my_list[count - i]
             my_list[count - i] = temp
