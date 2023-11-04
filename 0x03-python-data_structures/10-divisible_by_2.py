2f72a964b408819f80f1a38bbb31ab26194a6e62
kaphaein <cheriffadaly@github.com>
2023-11-04 12:23:30 -0700

    “lists”

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     boo = []
-    for i in my_list:
-        if i % 2 == 0:
-            boo += "True"
-        else:
-            boo += "False"
+    if my_list:
+        for i in my_list:
+            if i % 2 == 0:
+                boo += "True"
+            else:
+                boo += "False"
     return boo
