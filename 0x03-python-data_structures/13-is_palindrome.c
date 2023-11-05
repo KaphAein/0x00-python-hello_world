a313eca3f8ffdce2546c34bb0a589f2102e3ed9d
kaphaein <cheriffadaly@github.com>
2023-11-05 09:13:27 -0800

    “lists”

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -33,7 +33,7 @@ int is_palindrome(listint_t **head)
 	listint_t *fast = *head, *slow = *head, *cop = NULL, *current = *head;
 
 
-	if (*head == NULL && (*head)->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		return (1);
 	}
