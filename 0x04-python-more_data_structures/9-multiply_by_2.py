667f4b9dc0760a833a659d94c39a8e1f663e4203
kaphaein <cheriffadaly@github.com>
2023-11-08 07:28:33 -0800

    “More”

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    a_dictionary = [[x * 2 if x else x in row] for row in a_dictionary]
-    return a_dictionary
+return [[x * 2 for x in row] for row in a_dictionary]
