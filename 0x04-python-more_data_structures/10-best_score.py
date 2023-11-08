63cbd3bc0ff72da4182920c8cdb46e2bf099d4b4
kaphaein <cheriffadaly@github.com>
2023-11-08 10:00:25 -0800

    “More”

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     max_value = 0
     if a_dictionary is None:
         return None
-    for key, value in a_dictionary.items():
-        if value > max_value or max_value is None:
+    for value in a_dictionary.values():
+        if (value > max_value and isinstance(value, int)) or (max_value is None and isinstance(value, int)):
             max_value = value
     return max_value
