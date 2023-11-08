63e0accfb7970e79f4b760a07d776766bf793107
kaphaein <cheriffadaly@github.com>
2023-11-08 12:38:03 -0800

    “More”

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,15 @@
 def roman_to_int(roman_string):
     result = 0
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-           'C': 100, 'D': 500, 'M':1000}
-    if roman_string is None or not isinstance(roman_string, str):
+           'C': 100, 'D': 500, 'M': 1000}
+    s = roman_string
+    if s is None or not isinstance(s, str):
         return 0
-    for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and (rom[roman_string[i]] < rom[roman_string[i + 1]]):
-            result -= rom[roman_string[i]]
+
+    for i in range(len(s)):
+        if i + 1 < len(s) and rom[s[i]] < rom[s[i + 1]]:
+            result -= rom[s[i]]
         else:
-            result += rom[roman_string[i]]
+            result += rom[s[i]]
 
     return result
