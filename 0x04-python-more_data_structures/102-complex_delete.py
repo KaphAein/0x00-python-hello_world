16ce2164e58e40ceaa3f5319ad9e3c838e0d0faa
kaphaein <cheriffadaly@github.com>
2023-11-08 13:42:54 -0800

    “More”

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    del_key = []
     if a_dictionary and value:
         for key, val in a_dictionary.items():
             if val == value:
-                del val
+                del_key.append(key)
+
+        for key in del_key:
+            del a_dictionary[key]
         return a_dictionary
