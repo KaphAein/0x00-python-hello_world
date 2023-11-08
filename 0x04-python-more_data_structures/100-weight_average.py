d6acbb062e6047c6a9b12ecfb33884e016839f59
kaphaein <cheriffadaly@github.com>
2023-11-08 13:05:22 -0800

    “More”

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
-        return 0
     sum_score = 0
     agg = 0
-    for pair in my_list:
-        weight, score = pair
-        sum_score += score
-        agg += weight * score
-    return  agg / sum_score
+    if my_list is None:
+        return 0
+    else:
+        for pair in my_list:
+            weight, score = pair
+            sum_score += score
+            agg += weight * score
+        return agg / sum_score
