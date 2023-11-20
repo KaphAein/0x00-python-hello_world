63195f111713bbd24682a687573985b7356870e3
kaphaein <cheriffadaly@github.com>
2023-11-20 04:33:31 -0800

    “exception”

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+           count += 1
+           print("{}".format(my_list(i), end=""))
+    except IndexError:
+        pass
+    finally:
+        print()
+    return count
