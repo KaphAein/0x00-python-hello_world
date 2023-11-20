7eb1f8f047be53dc7b326ce720394a74fc5fe064
kaphaein <cheriffadaly@github.com>
2023-11-20 04:35:01 -0800

    “exception”

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,5 @@ def safe_print_list(my_list=[], x=0):
            print("{}".format(my_list(i), end=""))
     except IndexError:
         pass
-    finally:
-        print()
+    print()
     return count
