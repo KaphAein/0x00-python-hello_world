526e17af96aec3ae7e71dcc651bdbf3f5b6d391d
kaphaein <cheriffadaly@github.com>
2023-11-21 04:54:15 -0800

    “class”

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,7 +52,8 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                print(" " * self.__position[0])
+                for k in range(self.__position[0]):
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
