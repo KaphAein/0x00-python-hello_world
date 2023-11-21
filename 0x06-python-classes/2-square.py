ea8b01acd217adea3ba65867c2abb321c63b6a91
kaphaein <cheriffadaly@github.com>
2023-11-21 03:47:49 -0800

    “class”

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a Square"""
+    def __init__(self, size=0):
+        """Defines attribute size of a Square"""
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            raise ValueError("size must be >= 0")
