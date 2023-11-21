a9b3f931184fdb9775d6e1977b3ff36ac2c8734b
kaphaein <cheriffadaly@github.com>
2023-11-21 03:37:36 -0800

    “class”

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a Square"""
+    def __init__(self, size):
+        """Defines attribute size of a Square"""
+        self.size = size
