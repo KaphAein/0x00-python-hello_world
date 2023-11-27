997882f7c135cfa4d0b1306eca1429a22d42e4e2
kaphaein <cheriffadaly@github.com>
2023-11-27 06:23:16 -0800

    “MoreClasses”

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
 
     def __ret__(self):
         """return a string representation of the rectangle"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return f"Rectangle({self.__width}, {self.__height})"
