ce24f40adc178fc39f2b593f13f49055156a92ca
kaphaein <cheriffadaly@github.com>
2023-11-27 05:16:46 -0800

    “MoreClasses”

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         """Represents a setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,6 +32,6 @@ class Rectangle:
         """Represents a setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
