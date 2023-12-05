df7715b191d7f71c9dd5f42e8746cdc059e2d8cd
kaphaein <cheriffadaly@github.com>
2023-12-05 07:23:22 -0800

    “I/O”

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module that reads a utf-8 file"""
+
+
+def read_file(filename=""):
+    """function that reads a utf-8 file"""
+    with open("filename", 'r', encoding='utf-8') as file:
+        print(file.read())
