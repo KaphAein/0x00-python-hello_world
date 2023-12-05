f755cfa81e5ac052eda9b287f0cc9fcfb37b444e
kaphaein <cheriffadaly@github.com>
2023-12-05 10:39:56 -0800

    “I/O”

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,6 @@ class Student:
 
         def to_json(self, attrs=None):
         '''method to_json'''
-        if (type(attrs) == list and
-            all(type(ele) == str for ele in attrs)):
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
