227bed0ec6332b9e4252e3e36a70c95d51609a9a
kaphaein <cherifafadaly@gmail.com>
2024-02-05 05:56:19 -0800

    “inheritance”

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry():
+    """BaseGeometry Class"""
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer_validator method"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""Rectangle module"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class"""
+    def __init__(self, width, height):
+        """Rectangle instantiation"""
+        self.integer_validator("width", width)
+        self.width = width
+        self.integer_validator("height", height)
+        self.height = height
+        
