db89cdd64c30d98a2f2a88246b1717b91b1e11e7
kaphaein <cherifafadaly@gmail.com>
2024-02-05 07:16:32 -0800

    “inheritance”

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""MyInt module"""
+
+
+class MyInt(int):
+    """MyInt Class"""
+    
+    def __eq__(self, other):
+        """equal method"""
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+        """not equal"""
+        return int(self) == int(other)
