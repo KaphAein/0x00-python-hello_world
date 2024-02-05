3aa96a9342d5d1c67865fae3e0621efd916c5b52
kaphaein <cherifafadaly@gmail.com>
2024-02-05 07:09:41 -0800

    “inheritance”

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square module"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+    def __init__(self, size):
+        """Rectangle instantiation"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area method"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """str method"""
+        return "[Square] {}/{}".format(size, size)
