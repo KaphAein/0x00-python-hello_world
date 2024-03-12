a02a6456c258e730c65de07fcc21c05331059335
kaphaein <cherifafadaly@gmail.com>
2024-03-12 09:55:04 -0700

    “MYYSQLintro”

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name FROM second_list WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_list WHERE name != '' OR IS NOT NULL ORDER BY score DESC;
