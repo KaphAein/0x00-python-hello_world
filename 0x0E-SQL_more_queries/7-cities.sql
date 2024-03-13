43d11b8741ed8d25b0e76e8caeb1001b1d1a8236
kaphaein <cherifafadaly@gmail.com>
2024-03-13 13:19:45 -0700

    “MOREqueries”

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-       id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+       id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       FOREIGN KEY (state_id) REFERENCES TO states(id)
+       FOREIGN KEY (state_id) REFERENCES states(id)
 );
