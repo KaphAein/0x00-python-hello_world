0ce77c8e51baaf5d3d5a76bfda74833b8e187ebf
kaphaein <cherifafadaly@gmail.com>
2024-04-08 10:38:53 -0700

    “JSBasics”

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // function that executes x times a function.
 
-module.export.callMeMoby = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for(let i = 0; i < x; i++) {
      theFunction();
   }
