894a513243758fc89033a1b2abc6dab9dd251214
kaphaein <cherifafadaly@gmail.com>
2024-04-08 08:35:45 -0700

    “JSBasics”

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 
 const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (i = 0; i < 3; i++) {
-    console.log(phrases[i]);
+for (let i = 0; i < 3; i++) {
+  console.log(phrases[i]);
 }
