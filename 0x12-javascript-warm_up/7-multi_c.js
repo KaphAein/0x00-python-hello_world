1c41a20f9da2f0bc4f2439c41cf62324505a354c
kaphaein <cherifafadaly@gmail.com>
2024-04-08 08:51:45 -0700

    “JSBasics”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,6 +6,7 @@ const [,, ...args] = process.argv;
 if (isNaN(parseInt(args[0]))) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0;
-    console.log('C is fun' * parseInt(args[0]))
+	for (let i = 0; i < parseInt(args[0]); i++) {
+		console.log('C is fun');
+  }
 }
