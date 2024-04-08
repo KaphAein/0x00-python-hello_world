a2129edfb2a022c272ba8429877809f593a1c0e4
kaphaein <cherifafadaly@gmail.com>
2024-04-08 06:31:36 -0700

    “JSBasics”

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@
 const numArgs = process.argv.length;
 
 if (numArgs === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (numArgs === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
