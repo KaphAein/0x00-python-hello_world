aa446231db4ece5d5c3e1524e169995837e5cb04
kaphaein <cherifafadaly@gmail.com>
2024-04-08 10:48:55 -0700

    “JSBasics”

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -2,8 +2,6 @@
 // function that executes x times a function.
 
 exports.addMeMaybe = function (number, theFunction) {
-  for (let i = 0; i < number; i++) {
-    number++;
-    theFunction(number);
-  }
+  number++;
+  theFunction(number);
 };
