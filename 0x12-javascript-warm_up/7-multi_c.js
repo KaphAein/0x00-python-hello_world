83b13ff22ee7530f5b47e6ed870e6e003b2251dd
kaphaein <cherifafadaly@gmail.com>
2024-04-08 08:45:10 -0700

    “JSBasics”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints My number: <first argument converted in integer>
+
+const [,, ...args] = process.argv;
+
+if (isNaN(parseInt(args[0]))) {
+  console.log('Missing number of occurrences');
+} else {
+    for (let i = 0;
+    console.log('C is fun' * parseInt(args[0]))
+}
