9d4a44850adc5d666dbb5ca5ab8fe4d3b6a8bc55
kaphaein <cherifafadaly@gmail.com>
2024-04-08 10:02:16 -0700

    “JSBasics”

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments.
+const (,, ...args) = process.argv;
 
-if (process.argv.length <= 3) {
+if (args <= 1) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
+  const list = args.sort();
   console.log(list.reverse()[1]);
 }
