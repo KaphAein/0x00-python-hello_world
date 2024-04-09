089c9f321adaa353ae5e8447cba6ee0d446ce4ba
kaphaein <cherifafadaly@gmail.com>
2024-04-09 08:23:53 -0700

    “JSOOP”

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-exports.logMe = function (item) {
-  let n = 0;
-  return function(itemn) { console.log(n++ + ': ' + item); };
-};
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
