eb30ceca1ba0262499e4b350882e153bfc53f881
kaphaein <cherifafadaly@gmail.com>
2024-04-09 07:53:18 -0700

    “JSOOP”

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,8 +6,8 @@ module.exports = class Square extends require('./4-rectangle.js') {
 
   charPrint (c) {
     if (c === undefined) c = 'X';
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 };
