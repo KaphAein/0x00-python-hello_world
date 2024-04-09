cc57c95a1d43218a3306a7f10b330a0e3ed04847
kaphaein <cherifafadaly@gmail.com>
2024-04-09 09:04:19 -0700

    “JSOOP”

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const { dict } = require('./101-data.js');
+
+console.log(list);
+console.log(list.map((element, index) => element * index));
---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const a = process.argv[2];
+const b = process.argv[3];
+const c = process.argv[4];
+
+console.log(a + b + c);
