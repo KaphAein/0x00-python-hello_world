6d04d3557954a8fa30722455455aa7ab6a91f0df
kaphaein <cherifafadaly@gmail.com>
2024-04-09 09:27:43 -0700

    “JSOOP”

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
-const { dict } = require('./101-data.js');
+const dict = require('./101-data.js').dict;
+let newDict = {};
 
-console.log(list);
-console.log(list.map((element, index) => element * index));
+for (const key in dict) {
+  const value = dict[key];
+  newDict[value] = [];
+}
+for (const key in dict) {
+  const value = dict[key];
+  newDict[value].push(key);
+}
+for (const key in newDict) {
+  newDict[key].sort((a, b) => a - b);
+}
+console.log(newDict);
