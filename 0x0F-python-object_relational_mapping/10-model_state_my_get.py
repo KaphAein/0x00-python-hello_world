037c554fba583360e21900d52466bc6f35cc2fd1
kaphaein <cherifafadaly@gmail.com>
2024-04-12 11:03:09 -0700

    “ORM”

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).filter(State.name == argv[4])\
-                                 .first()
+                                .first()
 
     if state:
         print(state.id)
