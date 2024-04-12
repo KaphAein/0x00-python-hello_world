5c849eeeff9b65d4262b53e0b53c26376d1570ab
kaphaein <cherifafadaly@gmail.com>
2024-04-12 09:24:53 -0700

    “ORM”

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-        'SELECT cities.id, states.name, cities.name FROM cities'
-        'INNER JOIN states ON cities.state_id = states.id'
+        'SELECT cities.id, states.name, cities.name FROM cities '
+        'INNER JOIN states ON cities.state_id = states.id '
         'ORDER BY cities.id ASC')
     states = cur.fetchall()
     for state in states:
