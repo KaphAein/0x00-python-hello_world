05f1ae8aa21d7e4b836181651e0839f9fa66cd97
kaphaein <cherifafadaly@gmail.com>
2024-04-12 08:36:45 -0700

    “ORM”

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+    states = cur.fetchall()
+    for state in states:
+        if state[1].startswith('N'):
+            print(state)
+    cur.close()
+    db.close()
