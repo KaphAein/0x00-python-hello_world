4190242f8f8b3809fc36825620afe867d6cf55d6
kaphaein <cherifafadaly@gmail.com>
2024-04-12 09:42:26 -0700

    “ORM”

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-        'SELECT cities.id, cities.name FROM cities '
-        'INNER JOIN states ON cities.state_id = states.id '
-        'WHERE states.name = %s '
-        'ORDER BY cities.id ASC', (argv[4], ))
+        '''SELECT cities.id, cities.name FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC''', (argv[4], ))
     print(", ".join(map(lambda x: x[0], cur.fetchall())))
     cur.close()
     db.close()
