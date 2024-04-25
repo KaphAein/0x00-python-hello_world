fc87448608a18821a6d2af4a2737a3db9796544e
kaphaein <cherifafadaly@gmail.com>
2024-04-25 09:38:19 -0700

    “NETWORK0”

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,7 +11,8 @@ def find_peak(list_of_integers):
     start, end = 0, list_len - 1
     while start < end:
         mid = start + (end - start) // 2
-        if (int_list[mid] > int_list[mid - 1]) and (int_list[mid] > int_list[mid + 1]):
+        if (int_list[mid] > int_list[mid - 1])
+        and (int_list[mid] > int_list[mid + 1]):
             return int_list[mid]
         elif int_list[mid - 1] > int_list[mid + 1]:
             end = mid
