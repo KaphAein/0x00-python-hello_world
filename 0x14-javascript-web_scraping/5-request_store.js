0d1b91069a90aa38c3690da72952f8fc004f0fc2
kaphaein <cherifafadaly@gmail.com>
2024-05-21 21:26:06 +0000

    "JSON"

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,5 +3,4 @@ const fs = require('fs');
 const request = require('request');
 const [,, url, filePath] = process.argv;
 const fileStream = fs.createWriteStream(filePath);
-
 request(url).pipe(fileStream)
