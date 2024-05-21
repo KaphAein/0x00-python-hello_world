d08d04d787edfbb253fbce325479fcd71d771ada
kaphaein <cherifafadaly@gmail.com>
2024-05-21 20:57:47 +0000

    "JSON"

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,13 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/:id'
 const movieID = process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+request.get(url, (err, response, body) => {
+ if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    const movie = JSON.parse(body);
-    console.log(movie.title)
+    console.log('Error code: ' + response.statusCode);
   }
 });
