20734322fefc85c97fdb5476f84a9caac261f7aa
kaphaein <cherifafadaly@gmail.com>
2024-05-21 21:01:19 +0000

    "JSON"

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id'
 const movieID = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/${movieID}`'
 
 request.get(url, (err, response, body) => {
  if (err) {
